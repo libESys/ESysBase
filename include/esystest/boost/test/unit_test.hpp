206d50b0dd646f4724d27ff4b6fb16182ffd9af9
Michel Gillet <michel.r.gillet@gmail.com>
2015-11-07 06:54:29 +0200

    Gets the unit tests to build with visual c++ 2008.

---
+++ include/esystest/boost/test/unit_test.hpp
@@ -0,0 +1,27 @@
+/*!
+ * \file esystest/boost/test/unit_test.hpp
+ * \brief Version info for esystest
+ *
+ * \cond
+ *__legal_b__
+ *
+ *__legal_e__
+ * \endcond
+ *
+ */
+
+#ifndef __ESYSTEST_BOOST_TEST_UNIT_TEST_HPP__
+#define __ESYSTEST_BOOST_TEST_UNIT_TEST_HPP__
+
+namespace esystest
+{
+}
+
+#ifdef BOOST_TEST_MAIN 
+int main(int argc, char *argv[])
+{
+    return 0;
+}
+#endif
+
+#endif
---
+++ src/esystest_t/unittest.cpp
@@ -0,0 +1,34 @@
+/*!
+ * \file esystest_t/unittest.cpp
+ * \brief
+ *
+ * \cond
+ * __legal_b__
+ *
+ * Copyright (c) 2015 Michel Gillet
+ * Distributed under the wxWindows Library Licence, Version 3.1.
+ * (See accompanying file LICENSE_3_1.txt or
+ * copy at http://www.wxwidgets.org/about/licence)
+ *
+ * __legal_e__
+ * \endcond
+ *
+ */
+
+#define BOOST_TEST_MAIN
+
+#ifdef WIN32
+#include <vld.h>
+#endif
+
+#ifdef _MSC_VER
+#pragma warning (disable : 4996)
+#pragma warning (disable : 4985)
+#endif
+
+#include <boost/test/unit_test.hpp>
+
+#ifdef _MSC_VER
+#pragma warning (default : 4996)
+#pragma warning (default : 4985)
+#endif
