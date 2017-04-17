211e2b17776d15e23279a6d54229463a88d8b43e
Michel Gillet <michel.gillet@libesys.org>
2017-04-18 00:04:05 +0300

    Adds ESysBuild config file
    Change-Id: I52d26734651a38a193fe8e71e60f7f54f0354dd9

---
+++ esysbuild.cfg.py
@@ -0,0 +1,31 @@
+from esysbuild.lib import *
+from esysbuild.exe import *
+from esysbuild.buildfile import *
+
+class ESysTestBuildFile(BuildFile):
+    def __init__(self):
+        super().__init__()
+        self.SetBootStrapScript("config/bootstrap")
+        
+    def AddLibs(self, lib_mngr):   
+        vhw_hw=["vhw", "hw"]
+        
+        esystest=Lib("esystest")
+        esystest.AddGroupName(vhw_hw)
+        esystest.SetLinkCfg(Lib.LINK_LIBTOOL, "src/esystest/libesystest.la")
+        esystest.SetLinkCfg(Lib.LINK_INSTALLED, "-lesystest")
+        esystest.AddDependencies(["esys"])
+        lib_mngr.Add(esystest)
+        
+        esystest_t=Exe("esystest_t")
+        esystest_t.AddGroupName("vhw")
+        esystest_t.AddDependencies(["esys", "esystest"])
+        lib_mngr.Add(esystest_t)            
+        return 0
+    
+def GetBuildFile():
+    return ESysTestBuildFile()
+
+
+
+    
