b28bcfcdbf09dee7392ee7a8aa6240af780a30e6
Michel Gillet <michel.gillet@libesys.org>
2021-12-14 01:08:04 +0200

    chore: create conan package only for a version bump
    Ref #16

--- scripts/conan_publish_gitlab.sh
+++ scripts/conan_publish_gitlab.sh
@@ -6,9 +6,11 @@ echo "pwd = "`pwd`
 export CONAN_USER_HOME="$PWD/_conan"
 echo "CONAN_USER_HOME=$CONAN_USER_HOME"
 
+source ./scripts/conan_pkg_version.env
+
 cd build/conan
 
-CONAN_LOGIN_USERNAME=ci_user CONAN_PASSWORD=${CI_JOB_TOKEN} conan upload ESysTest/0.1.0@libesys/testing --all --remote=conan_project
+CONAN_LOGIN_USERNAME=ci_user CONAN_PASSWORD=${CI_JOB_TOKEN} conan upload ESysTest/${ESYSTEST_CONAN_PKG_VERSION}${1} --all --remote=conan_project
 if [ ! $? -eq 0 ]; then
    echo "${TXT_E}Failed to publish conan pkg to GitLab.${TXT_CLEAR}"
    exit 1
--- scripts/conan_publish_jfrogio.sh
+++ scripts/conan_publish_jfrogio.sh
@@ -6,6 +6,8 @@ echo "pwd = "`pwd`
 export CONAN_USER_HOME="$PWD/_conan"
 echo "CONAN_USER_HOME=$CONAN_USER_HOME"
 
+source ./scripts/conan_pkg_version.env
+
 cd build/conan
 
 echo "username $1"
@@ -15,7 +17,7 @@ if [ ! $? -eq 0 ]; then
    exit 1
 fi
 
-conan upload ESysTest/0.1.0@libesys/testing --all --remote=libesys_jfrogio
+conan upload ESysTest/${ESYSTEST_CONAN_PKG_VERSION}${3} --all --remote=libesys_jfrogio
 if [ ! $? -eq 0 ]; then
    echo "${TXT_E}Failed to publish conan pkg to LibEsys JFrog.io conan registry.${TXT_CLEAR}"
    exit 1
